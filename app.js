48a819fe0e82f20f48b1b807cd71135bc61ea1ef
sushantkumar23 <sushantkumar23@gmail.com>
2015-10-02 11:49:32 +0530

    Passport authentication added

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
+var authenticate = require('./routes/authenticate')(passport);
 
 var app = express();
 
@@ -17,14 +21,24 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(session({
+  secret: 'sushant'
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+//// Initialize Passport
+var initPassport = require('./passport-init');
+initPassport(passport);
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/auth', authenticate);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ passport-init.js
@@ -0,0 +1,67 @@
+var LocalStrategy   = require('passport-local').Strategy;
+var bCrypt = require('bcrypt-nodejs');
+//temporary data store
+var users = {};
+module.exports = function(passport){
+
+    // Passport needs to be able to serialize and deserialize users to support persistent login sessions
+    passport.serializeUser(function(user, done) {
+        //Tell passport which id to use for user
+        console.log('serializing user:', user.username);
+        return done(null, user.username);
+    });
+
+    passport.deserializeUser(function(username, done) {
+
+        return done(null, users[username]);
+
+    });
+
+    passport.use('login', new LocalStrategy({
+            passReqToCallback : true
+        },
+        function(req, username, password, done) {
+
+            //Check if user exists
+            if(!users[username]){
+                return done('This email-id is not registered', false);
+            }
+
+            //Check if password is valid
+            if(!isValidPassword(users[username], password)){
+                return done('Invalid password', false);
+            }
+            console.log('Successfully signed in.');
+            return done(null, users[username]);
+        }
+    ));
+
+    passport.use('signup', new LocalStrategy({
+                passReqToCallback : true // allows us to pass back the entire request to the callback
+            },
+            function(req, username, password, done) {
+
+                //Check if user already exists
+                if(users[username]){
+                    return done('Username already exists', false);
+                }
+
+                //Add user to database
+                users[username] = {
+                    username: username,
+                    password: createHash(password)
+                };
+                return done(null, true);
+
+            })
+    );
+
+    var isValidPassword = function(user, password){
+        return bCrypt.compareSync(password, user.password);
+    };
+    // Generates hash using bCrypt
+    var createHash = function(password){
+        return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+    };
+
+};
